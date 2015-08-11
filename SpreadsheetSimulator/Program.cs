ba584e890156d852c046c1ac80e8252ef4edeec6
Andrey Tretyak <andrey.o.tretyak@gmail.com>
2015-08-11 14:14:43 +0300

    + SpreadsheetProcessor - class lib with base logic for spreadsheet evaluation but without expression parser. + SpreadsheetSimulator - console app for SpreadsheetProcessor demonstration.

---
+++ SpreadsheetSimulator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpreadsheetProcessor;
+
+namespace SpreadsheetSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = new SpreadsheetSource(File.Open("input.txt", FileMode.Open));
+        }
+    }
+}
