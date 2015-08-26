e0455bf16810bf824939e6617edb4eb3061d6822
Andrey Tretyak <andrey.o.tretyak@gmail.com>
2015-08-26 10:15:04 +0300

    Added missing file ParallelProcesssingStrategy

---
+++ Spreadsheet.Core/ProcessingStrategies/ParallelProcessingStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spreadsheet.Core.Cells;
+
+namespace Spreadsheet.Core
+{
+    public class ParallelProcessingStrategy : IProcessingStrategy
+    {
+        public IEnumerable<object> Evaluate(ISpreadsheet spreadsheet, Func<ICell,object> evaluation)
+        {
+           return spreadsheet.AsParallel()
+                             .WithDegreeOfParallelism(spreadsheet.ColumnCount)
+                             .OrderBy(e => e.Address.Row)
+                             .ThenBy(e => e.Address.Column)
+                             .Select(evaluation);
+        }
+    }
+}
